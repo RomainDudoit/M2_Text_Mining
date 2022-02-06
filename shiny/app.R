17ba45854061bcc556c8ddea891e2da297c8c5ed
DESKTOP-0RCHEMJ\abdel <abapiste@hotmail.fr>
2022-02-06 22:05:26 +0100

    ajout mise à  jour

--- shiny/app.R
+++ shiny/app.R
@@ -15,10 +15,16 @@ library(corrplot)
 library(questionr)
 library(RMySQL)
 library(plotly)
+library(httr)
+library(jsonlite)
+library(stringr)
+library(RMySQL)
+library(utf8)
 
 
 eval(parse("ui.R", encoding="UTF-8"))
 eval(parse("serv.R", encoding="UTF-8"))
+eval(parse("mise_a_jour.R",encoding = "UTF-8"))
 
 shinyApp(ui = ui, server = server)
 
